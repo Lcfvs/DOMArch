257eb09e525fc6ad2c15e37631c7fef9a5613df4
Lcf.vs <lcf.vs@hotmail.com>
2016-07-05 15:49:32 +0200

    Update cli.php

--- 127.0.0.1/entry-points/service/cli/cli.php
+++ 127.0.0.1/entry-points/service/cli/cli.php
@@ -3,7 +3,7 @@ chdir(__DIR__ . '/..');
 
 require_once 'vendor/autoload.php';
 
-Lib\Config::parse(__DIR__ . '/config.json', true);
+Lib\Config::parse('config.json', true);
 Lib\Bootstrap\Cli::bootstrap();
 
 if ($argv[0] === 'cli' . DIRECTORY_SEPARATOR . 'cli.php') {
